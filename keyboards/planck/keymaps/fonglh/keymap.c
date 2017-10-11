7395e224d8bbf5944ebbfbc756ffecfc520226a4
fonglh <fonglh@gmail.com>
2017-10-11 22:02:09 +0800

    Double tap LOWER to enter NAV layer.
    Checks if Windows or Mac mods are activated to determine which Nav layer
    to activate.

--- keyboards/planck/keymaps/fonglh/keymap.c
+++ keyboards/planck/keymaps/fonglh/keymap.c
@@ -25,7 +25,8 @@ enum planck_keycodes {
 };
 
 enum {
-  TD_QUOTE_ENTER = 0
+  TD_QUOTE_ENTER = 0,
+  TD_LOWER_NAV
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+------+------+------|------+------+------+------+------+------|
  * | (/Lsft|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |)/Rsft|
  * |-------+------+------+------+------+------+------+------+------+------+------+------|
- * |Esc/Ctl| Num  | Alt  | GUI  |Lower | Space| Bksp | Raise|  Up  |   [  |  ]   |Enter |
+ * |Esc/Ctl| Num  | Alt  | GUI  |Low/Nav| Space| Bksp | Raise|  Up  |   [  |  ]   |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS},
   {CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, TD(TD_QUOTE_ENTER)},
   {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
-  {CTL_T(KC_ESC), TT(_NUM), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_UP, KC_LBRC, KC_RBRC,   KC_ENT}
+  {CTL_T(KC_ESC), TT(_NUM), KC_LALT, KC_LGUI, TD(TD_LOWER_NAV),   KC_SPC,  KC_BSPC,  RAISE,   KC_UP, KC_LBRC, KC_RBRC,   KC_ENT}
 },
 
 /* Dvorak
@@ -56,14 +57,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+------+------+------|------+------+------+------+------+------|
  * | (/Lsft|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |)/Rsft|
  * |-------+------+------+------+------+------+------+------+------+------+------+------|
- * |Esc/Ctl| Num  | Alt  | GUI  |Lower |    Space    |Raise |  Up  |   [  |   ]  |Enter |
+ * |Esc/Ctl| Num  | Alt  | GUI  |Low/Nav|Space| Bksp    |Raise |  Up  |   [  |   ]  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
   {CTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH},
   {KC_LSPO, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSPC},
-  {CTL_T(KC_ESC), TT(_NUM), KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_BSPC,  RAISE,   KC_UP, KC_LBRC, KC_RBRC,   KC_ENT}
+  {CTL_T(KC_ESC), TT(_NUM), KC_LALT, KC_LGUI, TD(TD_LOWER_NAV),   KC_SPC,  KC_BSPC,  RAISE,   KC_UP, KC_LBRC, KC_RBRC,   KC_ENT}
 },
 
 /* Lower
@@ -74,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |  Nav |      |      | Del  |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      | Del  |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
   {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LBRC, KC_RBRC, KC_EQL},
   {_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE},
   {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______},
-  {_______, _______, _______, NAV, _______, _______, KC_DEL, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
+  {_______, _______, _______, _______, _______, _______, KC_DEL, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
 /* Raise
@@ -122,8 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
-/* NavMac -- Nav layer for Macs to navigate between tabs, activate by pressing LOWER first, then using the same thumb to press the key to the left.
- *           This gives it a very similar shape to Cmd + <number> on a Mac keyboard.
+/* NavMac -- Nav layer for Macs to navigate between tabs, enter by double tapping LOWER.
  * ,-----------------------------------------------------------------------------------.
  * |CmdGrv|Cmd-1 |Cmd-2 |Cmd-3 |Cmd-4 |Cmd-5 | Cmd-6 |Cmd-7 |Cmd-8 | Cmd-9|      |      |
  * |------+------+------+------+------+--------------+------+------+------+------+------|
@@ -219,28 +219,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case NAV:
-      if (record->event.pressed) {
-        // Windows GUI/ALT layout, assume Windows nav shortcuts.
-        if(keymap_config.swap_lalt_lgui == 1 && keymap_config.swap_ralt_rgui == 1) {
-          layer_on(_NAV);
-        } else {
-          layer_on(_NAVMAC);
-        }
-      } else {
-        layer_off(_NAV);
-        layer_off(_NAVMAC);
-      }
-      return false;
-      break;
   }
   return true;
+}
+
+void dance_nav_on(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    layer_on(_LOWER);
+  } else {
+    // Windows GUI/ALT layout, assume Windows nav shortcuts.
+    if(keymap_config.swap_lalt_lgui == 1 && keymap_config.swap_ralt_rgui == 1) {
+      layer_on(_NAV);
+    } else {
+      layer_on(_NAVMAC);
+    }
+  }
+}
+
+void dance_nav_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    layer_off(_LOWER);
+  } else {
+    layer_off(_NAV);
+    layer_off(_NAVMAC);
+  }
 }
 
 // Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for quote, twice for Enter
-  [TD_QUOTE_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_ENT)
+  [TD_QUOTE_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_ENT),
+  [TD_LOWER_NAV] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_nav_on, dance_nav_reset)
 };
 
 // Supposed to use this with ACTION_FUNCTION(n) in a keymap but it didn't work.
